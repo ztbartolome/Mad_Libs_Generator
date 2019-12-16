df03ec7ec6c0564b4901f54e6a6d59598378f2e9
ztbartolome <ztbartolome@gmail.com>
2019-12-15 20:21:33 -0500

    fixed some skeleton code

--- text_processor.py
+++ text_processor.py
@@ -10,22 +10,31 @@ def train():
 
 class MadLibs(object):
     def __init__(self):
-        self.raw    # or maybe it would be better to store a list of tokens
-        self.word_replacements = []  # will be a list of dictionaries of the form {'og':(word,tag), 'new':word}
+        self.raw    # may be unnecessary idk
+        self.tagged_tokens
+        self.word_replacements = {}  # will be a list of dictionaries of the form {(word, tag): replacement}
+
+    def tag_passage(self):
+        """Tag the tokens in the passage"""
+        pass
 
     def process_passage(self):
         """
         Takes a string with the raw text of a passage and removes random nouns/verbs/adjectives/adverbs,
         storing each removed word and its tag
         """
-        tagged_tokens = []  # placeholder
 
-        for (token, tag) in tagged_tokens:
+        for (token, tag) in self.tagged_tokens:
             if tag in tags_to_replace and random() > .5:
-                self.word_replacements.append({'og': (token, tag)})
-
+                self.word_replacements[(token, tag)] = None
 
     def replace(self, replacements):
-        """"""
-        for i in range(self.word_replacements):
-            self.word_replacements[i]['new'] = replacements[i]
+        """Accepts a list of words entered by the user and replaces the corresponding words in the passage"""
+        i = 0
+        for key in self.word_replacements:
+            self.word_replacements[key] = replacements[i]
+            i += 1
+        for token in self.tagged_tokens():
+            if token in self.word_replacements:
+                # to do: replace the word with self.word_replacements[token]
+                pass
