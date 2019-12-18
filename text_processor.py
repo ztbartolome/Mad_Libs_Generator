3f93f6164f8a7652e111800f73ef074d318c274a
ztbartolome <ztbartolome@gmail.com>
2019-12-17 22:44:31 -0500

    made function to determine whether verbs are transitive

--- text_processor.py
+++ text_processor.py
@@ -1,4 +1,5 @@
 from random import random
+from string import punctuation
 
 
 tags_to_replace = []    # put the tags we want to replace in here
@@ -12,7 +13,7 @@ class MadLibs(object):
     def __init__(self):
         self.raw    # may be unnecessary idk
         self.tagged_tokens
-        self.word_replacements = {}  # will be a list of dictionaries of the form {(word, tag): replacement}
+        self.word_replacements = {}  # will be a dictionary of the form {(word, tag): replacement}
 
     def tag_passage(self):
         """Tag the tokens in the passage"""
@@ -38,3 +39,34 @@ class MadLibs(object):
             if token in self.word_replacements:
                 # to do: replace the word with self.word_replacements[token]
                 pass
+
+    def determine_transitive(self):
+        """Adds '-T' or '-IT' to the end of each verb tag depending on whether it is transitive or intransitive"""
+        # for (token, tag) in self.tagged_tokens:
+        #     transitive =
+        #     if tag[:2] == 'VB':
+
+        i = 0
+        verb_index = -1
+        while i < len(self.tagged_tokens):
+            current_tag = self.tagged_tokens[i][1]
+            if current_tag[:2] == 'VB':
+                if verb_index > -1:
+                    # mark the previous verb as intransitive
+                    self.append_to_tag(verb_index, '-IT')
+                verb_index = i
+            elif current_tag[:2] == 'NN' and verb_index > -1:
+                # mark the most recent verb as transitive
+                self.append_to_tag(verb_index, '-T')
+                verb_index = -1
+            elif (current_tag == 'IN' or current_tag == 'CC' or current_tag in punctuation)and verb_index > -1:
+                # mark the most recent verb as intransitive
+                self.append_to_tag(verb_index, '-IT')
+                verb_index = -1
+            i += 1
+
+    def append_to_tag(self, index, end):
+        """Appends some string to the end of the tag at the given index of self.tagged_tokens"""
+        self.tagged_tokens[index] = (self.tagged_tokens[index][0], self.tagged_tokens[index][1] + end)
+
+
